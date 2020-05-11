488145c537c647338e782c17965aaaa7e6087660
waljqiang <waljqiang@163.com>
2020-05-11 17:38:15 +0800

    mcrypt_module_open->openssl

--- src/Class/AES.php
+++ src/Class/AES.php
@@ -16,19 +16,19 @@ class AES implements EncodeInterface{
 			//获得16位随机字符串，填充到明文之前
 			$random = $this->getRandomStr();
 			$text = $random . pack("N", strlen($text)) . $text;
-			// 网络字节序
-			$size = @mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-			$module = @mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-			$iv = substr($this->key, 0, 16);
 			//使用自定义的填充方式对明文进行补位填充
 			$pkc_encoder = new PKCS7Encode;
 			$text = $pkc_encoder->encode($text);
+			// 网络字节序
+			/*$size = @mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+			$module = @mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+			$iv = substr($this->key, 0, 16);
 			@mcrypt_generic_init($module, $this->key, $iv);
 			//加密
 			$encrypted = mcrypt_generic($module, $text);
 			@mcrypt_generic_deinit($module);
-			@mcrypt_module_close($module);
-
+			@mcrypt_module_close($module);*/
+			$encrypted = openssl_encrypt($text, 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA,substr($this->key, 0, 16));
 			return base64_encode($encrypted);
 		}catch(\Exception $e){
 			throw new Exception("AES encode failure", -1);	
@@ -39,14 +39,15 @@ class AES implements EncodeInterface{
 		try {
 			//使用BASE64对需要解密的字符串进行解码
 			$ciphertext_dec = base64_decode($data);
-			$module = @mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+			/*$module = @mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
 			$iv = substr($this->key, 0, 16);
 			@mcrypt_generic_init($module, $this->key, $iv);
 
 			//解密
 			$decrypted = @mdecrypt_generic($module, $ciphertext_dec);
 			@mcrypt_generic_deinit($module);
-			@mcrypt_module_close($module);
+			@mcrypt_module_close($module);*/
+			$decrypted = openssl_decrypt($ciphertext_dec, 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA,substr($this->key, 0, 16));
 		} catch (Exception $e) {
 			throw new \Exception("AES decrypt failure", -1);
 		}
